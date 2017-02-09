7a2e255892d05feb663415bfcc3e78fe5119f840
Eric Rasche <rasche.eric@gmail.com>
2017-02-09 03:26:39 +0000

    New map, random lighting

---
+++ maps/fjell.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git checkout 9c003a9d5f93d53e6d40e296702576e2e5898b21
+python redeclipse/cli/voxel_3.py maps/empty-night.mpz ~/.redeclipse/maps/fjell.mpz
--- maps/sandvika.sh
+++ maps/sandvika.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 git checkout 70471cd1947fd14c022f321cfd6ed9d3d51a3850
-python redeclipse/cli/voxel_3.py maps/empty2.mpz ~/.redeclipse/maps/test.mpz
+python redeclipse/cli/voxel_3.py maps/empty2.mpz ~/.redeclipse/maps/sandvika.mpz
