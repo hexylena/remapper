009d2dd077151e80edb79daf027f76799b3189e4
E Rasche <hxr@hx42.org>
2017-09-10 14:37:28 +0000

    testing sortof working

---
+++ tests/test_base.py
@@ -0,0 +1,5 @@
+import unittest
+
+# class TestRegisterNewInstructor(unittest.TestCase):
+    # def test_a(self):
+        # self.assertEqual(4, 4)
---
+++ tests/test_import.py
@@ -0,0 +1,52 @@
+
+def test_imports():
+    import redeclipse
+    import redeclipse.aftereffects
+    import redeclipse.cli
+    import redeclipse.cli.cfg_gen
+    import redeclipse.cli.from_json
+    import redeclipse.cli.iso
+    # import redeclipse.cli.mirror_2
+    # import redeclipse.cli.mirror_4
+    import redeclipse.cli.mod_cubes
+    # import redeclipse.cli.scaffold
+    # import redeclipse.cli.show_cubes
+    # import redeclipse.cli.snow_forest
+    # import redeclipse.cli.snow_forest2d
+    import redeclipse.cli.t_mv
+    import redeclipse.cli.t_textures
+    import redeclipse.cli.t_town
+    import redeclipse.cli.t_vector
+    import redeclipse.cli.terrain
+    import redeclipse.cli.to_json
+    import redeclipse.cli.voxel_1
+    import redeclipse.cli.voxel_2
+    import redeclipse.cli.voxel_3
+    import redeclipse.cli.voxel_4
+    import redeclipse.cli.voxel_5
+    import redeclipse.entities
+    import redeclipse.entities.model
+    import redeclipse.entities.weapon
+    import redeclipse.enums
+    import redeclipse.lighting
+    import redeclipse.magicavoxel
+    import redeclipse.objects
+    import redeclipse.prefabs
+    import redeclipse.prefabs.construction_kit
+    import redeclipse.prefabs.distributions
+    import redeclipse.prefabs.orientations
+    import redeclipse.prefabs.vector
+    import redeclipse.skybox
+    import redeclipse.textures
+    import redeclipse.textures.basetex
+    import redeclipse.textures.dull
+    import redeclipse.textures.dull_theme
+    import redeclipse.textures.minecraft
+    import redeclipse.textures.minecraft_theme
+    import redeclipse.textures.paper
+    import redeclipse.textures.paper_theme
+    import redeclipse.textures.primary
+    import redeclipse.textures.primary_theme
+    import redeclipse.upm
+    import redeclipse.vec
+    import redeclipse.voxel
